5f469421dc54d2ca15a0c33971f15ffe65607e34
Spencer Barbre <spencerbarbre@gmail.com>
2025-04-30 21:17:22 -0600

    corrected folder location error

--- vite.config.js
+++ vite.config.js
@@ -8,29 +8,29 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve("D:/School/Spring 2025/New folder", "src/index.html"),
+        main: resolve("D:/School/Spring 2025/New folder/repo-W1", "src/index.html"),
         cart: resolve(
-          "D:/School/Spring 2025/New folder",
+          "D:/School/Spring 2025/New folder/repo-W1",
           "src/cart/index.html",
         ),
         checkout: resolve(
-          "D:/School/Spring 2025/New folder",
+          "D:/School/Spring 2025/New folder/repo-W1",
           "src/checkout/index.html",
         ),
         product1: resolve(
-          "D:/School/Spring 2025/New folder",
+          "D:/School/Spring 2025/New folder/repo-W1",
           "src/product_pages/cedar-ridge-rimrock-2.html",
         ),
         product2: resolve(
-          "D:/School/Spring 2025/New folder",
+          "D:/School/Spring 2025/New folder/repo-W1",
           "src/product_pages/marmot-ajax-3.html",
         ),
         product3: resolve(
-          "D:/School/Spring 2025/New folder",
+          "D:/School/Spring 2025/New folder/repo-W1",
           "src/product_pages/northface-alpine-3.html",
         ),
         product4: resolve(
-          "D:/School/Spring 2025/New folder",
+          "D:/School/Spring 2025/New folder/repo-W1",
           "src/product_pages/northface-talus-4.html",
         ),
       },
