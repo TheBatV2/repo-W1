9a3a6e86e0b3375eee9a7691a718af8de58cf2dc
Spencer Barbre <spencerbarbre@gmail.com>
2025-04-30 20:58:28 -0600

    corrected errors in index.html with extra <li> tag set

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,22 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve("D:/School/Spring 2025/New folder", "src/index.html"),
-        cart: resolve("D:/School/Spring 2025/New folder", "src/cart/index.html"),
-        checkout: resolve("D:/School/Spring 2025/New folder", "src/checkout/index.html"),
+        cart: resolve(
+          "D:/School/Spring 2025/New folder",
+          "src/cart/index.html",
+        ),
+        checkout: resolve(
+          "D:/School/Spring 2025/New folder",
+          "src/checkout/index.html",
+        ),
         product1: resolve(
           "D:/School/Spring 2025/New folder",
           "src/product_pages/cedar-ridge-rimrock-2.html",
         ),
-        product2: resolve("D:/School/Spring 2025/New folder", "src/product_pages/marmot-ajax-3.html"),
+        product2: resolve(
+          "D:/School/Spring 2025/New folder",
+          "src/product_pages/marmot-ajax-3.html",
+        ),
         product3: resolve(
           "D:/School/Spring 2025/New folder",
           "src/product_pages/northface-alpine-3.html",
