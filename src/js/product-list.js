ef8a2f3fb8608dddb59c0d77d38221461cc16782
Spencer Barbre <spencerbarbre@gmail.com>
2025-05-28 21:35:59 -0600

    did weekly project, need to correct footer/header in product-list
    Co-authored-by: tarydenise <tarydenise@users.noreply.github.com>
    Co-authored-by: Hared Albancando Robles <ealbanca@users.noreply.github.com>

---
+++ src/js/product-list.js
@@ -0,0 +1,9 @@
+import  productList  from "./productList.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+const category = getParam("category");
+productList(".product-list", category);
+
+
+
