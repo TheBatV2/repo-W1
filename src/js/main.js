fcc79e74671ab43313c27b02e9793dc96e6da218
Spencer Barbre <spencerbarbre@gmail.com>
2025-06-05 06:19:26 -0600

    changed js files to be correct

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-import productList from "./productList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-await loadHeaderFooter();
-
-productList(".product_list", "tents");
+loadHeaderFooter();
--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,9 +1,7 @@
-import  productList  from "./productList.mjs";
+import productList from "./productList.mjs";
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 
-await loadHeaderFooter();
+loadHeaderFooter();
 const category = getParam("category");
-productList(".product_list", category);
-
-
+productList(".product-list", category);
 
